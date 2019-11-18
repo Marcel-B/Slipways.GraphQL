8c3b3d0e23c3c6948c89ae77e4bc398598fa277b
Marcel Benders <marcel.benders@cleverbridge.com>
2019-11-18 11:41:51 +0100

    (0008_Mutations) (0008) Add class for mutation

---
+++ Slipways.GraphQL/Data/GraphQLQueries/AppMutation.cs
@@ -0,0 +1,12 @@
+﻿using GraphQL.Types;
+
+namespace com.b_velop.Slipways.GraphQL.Data.GraphQLQueries
+{
+    public class AppMutation : ObjectGraphType
+    {
+        public AppMutation()
+        {
+
+        }
+    }
+}
