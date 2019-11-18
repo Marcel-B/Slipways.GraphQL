8aadb22980700db2d1013d34075222f566dcb863
Marcel Benders <marcel.benders@cleverbridge.com>
2019-11-18 13:06:44 +0100

    (0008_Mutations) add mutation for slipway

--- Slipways.GraphQL/Data/GraphQLQueries/AppMutation.cs
+++ Slipways.GraphQL/Data/GraphQLQueries/AppMutation.cs
@@ -1,12 +1,34 @@
-﻿using GraphQL.Types;
+﻿using com.b_velop.Slipways.GraphQL.Data.GraphQLInputTypes;
+using com.b_velop.Slipways.GraphQL.Data.GraphQLTypes;
+using com.b_velop.Slipways.GraphQL.Data.Models;
+using com.b_velop.Slipways.GraphQL.Data.Repositories;
+using GraphQL.Types;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace com.b_velop.Slipways.GraphQL.Data.GraphQLQueries
 {
     public class AppMutation : ObjectGraphType
     {
-        public AppMutation()
+        public AppMutation(
+            IRepositoryWrapper rep,
+            ILogger<AppMutation> logger)
         {
+            Name = "Mutation";
 
+            FieldAsync<SlipwayType>(
+                "createSlipway",
+                "Creates a new Slipway",
+                new QueryArguments(
+                    new QueryArgument<SlipwayInputType> { Name = "slipway" }),
+                resolve: async ctx =>
+                {
+                    var slipway = ctx.GetArgument<Slipway>("slipway");
+
+                    slipway.Id = Guid.NewGuid();
+
+                    return await rep.Slipway.Create(slipway);
+                });
         }
     }
 }
