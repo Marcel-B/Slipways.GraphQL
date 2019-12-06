4c1cb6c9c2282b1826a63ae5571d75522b71c313
Marcel Benders <marcel.benders@cleverbridge.com>
2019-12-06 09:56:41 +0100

    (dev) Add Test controller

---
+++ Slipways.GraphQL/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Slipways.GraphQL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get() => Ok();
+    }
+}
