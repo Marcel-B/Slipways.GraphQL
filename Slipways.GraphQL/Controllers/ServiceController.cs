9133cf47c87ec182b50a9dbfb4d6e9e05cd00013
Marcel Benders <marcel.benders@icloud.com>
2019-12-06 09:16:56 +0100

    (dev) auth controller

--- Slipways.GraphQL/Controllers/ServiceController.cs
+++ Slipways.GraphQL/Controllers/ServiceController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using com.b_velop.Slipways.GrQl.Data.Models;
 using com.b_velop.Slipways.GrQl.Data.Repositories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
