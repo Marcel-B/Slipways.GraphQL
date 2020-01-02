6ebb49ca3f279a10497f8f714cebf27e5053d579
Marcel Benders <marcel.benders@cleverbridge.com>
2020-01-02 15:37:56 +0100

    (dev) Update Slipways.Data and refactoring

--- Slipways.GraphQL/Data/GraphQLTypes/WaterType.cs
+++ Slipways.GraphQL/Data/GraphQLTypes/WaterType.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using com.b_velop.Slipways.Data.Contracts;
+using com.b_velop.Slipways.Data.Helper;
 using com.b_velop.Slipways.Data.Models;
 using GraphQL.DataLoader;
 using GraphQL.Types;
@@ -10,7 +11,7 @@ namespace com.b_velop.Slipways.GrQl.Data.GraphQLTypes
     public class WaterType : ObjectGraphType<Water>
     {
         public WaterType(
-            IRepositoryWrapper rep,
+            IRepositoryWrapper repository,
             IDataLoaderContextAccessor accessor)
         {
             Name = nameof(Water);
@@ -20,30 +21,30 @@ namespace com.b_velop.Slipways.GrQl.Data.GraphQLTypes
             Field(_ => _.Longname);
 
             FieldAsync<ListGraphType<SlipwayType>, IEnumerable<Slipway>>(
-                "Slipways",
-                "Slipways",
-                resolve: async ctx =>
+                Cache.Slipways,
+                Cache.Slipways,
+                resolve: async context =>
                 {
-                    var loader = accessor.Context.GetOrAddCollectionBatchLoader<Guid, Slipway>("GetSlipwaysByWaterId", rep.Slipway.GetSlipwayByWaterIdAsync);
-                    return await loader.LoadAsync(ctx.Source.Id);
+                    var loader = accessor.Context.GetOrAddCollectionBatchLoader<Guid, Slipway>("GetSlipwaysByWaterId", repository.Slipway.GetSlipwayByWaterIdAsync);
+                    return await loader.LoadAsync(context.Source.Id);
                 });
 
             FieldAsync<ListGraphType<StationType>, IEnumerable<Station>>(
-                "Stations",
-                "Stations",
-                resolve: async ctx =>
+                Cache.Stations,
+                Cache.Stations,
+                resolve: async context =>
                 {
-                    var loader = accessor.Context.GetOrAddCollectionBatchLoader<Guid, Station>("GetStationsByWaterId", rep.Station.GetStationsByWaterIdAsync);
-                    return await loader.LoadAsync(ctx.Source.Id);
+                    var loader = accessor.Context.GetOrAddCollectionBatchLoader<Guid, Station>("GetStationsByWaterId", repository.Station.GetStationsByWaterIdAsync);
+                    return await loader.LoadAsync(context.Source.Id);
                 });
 
             FieldAsync<ListGraphType<PortType>, IEnumerable<Port>>(
                 "Ports",
                 "Ports",
-                resolve: async ctx =>
+                resolve: async context =>
                 {
-                    var loader = accessor.Context.GetOrAddCollectionBatchLoader<Guid, Port>("GetPortsByWaterId", rep.Port.GetPortsByWaterIdAsync);
-                    return await loader.LoadAsync(ctx.Source.Id);
+                    var loader = accessor.Context.GetOrAddCollectionBatchLoader<Guid, Port>("GetPortsByWaterId", repository.Port.GetPortsByWaterIdAsync);
+                    return await loader.LoadAsync(context.Source.Id);
                 });
         }
     }
