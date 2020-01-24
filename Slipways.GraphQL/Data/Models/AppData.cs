dfb9a874f12bdda5f4457f73aeb55658d5ed3af9
Marcel Benders <marcel.benders@icloud.com>
2020-01-24 12:07:06 +0100

    (dev) Add Manufacturers and Extras to AppData

--- Slipways.GraphQL/Data/Models/AppData.cs
+++ Slipways.GraphQL/Data/Models/AppData.cs
@@ -10,5 +10,7 @@ namespace com.b_velop.Slipways.GrQl.Data.Models
         public IEnumerable<Water> Waters { get; set; }
         public IEnumerable<Service> Services { get; set; }
         public IEnumerable<Station> Stations { get; set; }
+        public IEnumerable<Manufacturer> Manufacturers { get; set; }
+        public IEnumerable<Extra> Extras { get; set; }
     }
 }
