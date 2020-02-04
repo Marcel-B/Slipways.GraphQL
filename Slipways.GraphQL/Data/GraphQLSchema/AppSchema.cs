ad12f570025cd190afcb8f4d5fbba48c54e48226
Marcel Benders <marcel.benders@cleverbridge.com>
2020-02-04 17:09:44 +0100

    (dev) Update Data, Add Subscriptions

--- Slipways.GraphQL/Data/GraphQLSchema/AppSchema.cs
+++ Slipways.GraphQL/Data/GraphQLSchema/AppSchema.cs
@@ -10,6 +10,7 @@ namespace com.b_velop.Slipways.GrQl.Data.GraphQLSchema
             IDependencyResolver resolver)  : base(resolver)
         {
             Query = resolver.Resolve<AppQuery>();
+            Subscription = resolver.Resolve<AppQuery>();
             //Mutation = resolver.Resolve<AppMutation>();
         }
     }
